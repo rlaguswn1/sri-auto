b5c655a8ab7ad9c5b3b213b7fb7f9f427cd97065
khj <khj9872100@gmail.com>
2022-06-28 17:32:03 +0900

    테스트 한 번 해보고 효과높이기 추가

--- login-com.py
+++ login-com.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from datetime import datetime
 
 # id = input("id를 입력하세요: ")
 # pw = input("pw를 입력하세요: ")
@@ -44,10 +46,16 @@ apply_nm.send_keys('test')
 # 경력 - 경력
 career = dvr.find_element(By.CSS_SELECTOR, '#line_detail_0 > div.fulldata > div:nth-child(2) > div.area_data.type_txt > span:nth-child(2) > label')
 career.click()
+career_year=dvr.find_element(By.XPATH, '//*[@id="career_min_0"]/option[21]')
+career_year.click()
 
 # 근무형태
 jtype= dvr.find_element(By.CSS_SELECTOR, '#job_type_list > li:nth-child(1) > span > label')
 jtype.click()
+
+# 급여 - 면접 후 결정
+pay = dvr.find_element(By.XPATH, '//*[@id="salary_category"]/option[7]')
+pay.click()
 
 # 직무 추가
 ad=dvr.find_element(By.CSS_SELECTOR, '#jobCategory > div.area_data > div.selected_item > button.btn_add')
@@ -60,10 +68,23 @@ jsearch.send_keys('qa')
 # qa 선택
 time.sleep(0.5)
 jsearch_qa = dvr.find_element(By.XPATH, '//*[@id="job_cate_search_99"]')
-jsearch_qa.click()
+# click()이 안되면 send.keys 해보자. 쟤가 아마... input 타입이라 클릭이 안되고 send.keys가 된거같기도 해
+jsearch_qa.send_keys(Keys.SPACE)
 jsearch_submit = dvr.find_element(By.CSS_SELECTOR, '#jobCategory > div.area_data > div.wrap_layer > div > div.area_btn > button.btnSizeL.colorBlue.btn_complete')
 jsearch_submit.click()
 
+# 공고 제목 입력
+rec_nm=dvr.find_element(By.CSS_SELECTOR, '#title')
+# 월+일 4자로 보여주기 (ex. 0628)
+a=(str(datetime.today().month)+str(datetime.today().day)).zfill(4)
+rec_nm.clear()
+rec_nm.send_keys(str(a)+'사람인 테스트 공고입니다. 지원 불가합니다.')
 
+# 템플릿 반영
+tem_apply=dvr.find_element(By.CSS_SELECTOR, '#template_insert')
+tem_apply.click()
 
+# 등록
+submit = dvr.find_element(By.CSS_SELECTOR, '#content > div.wrap_recruit_frm > div.wrap_bottom_btn > button')
+submit.click()
 
