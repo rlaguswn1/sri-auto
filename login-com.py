750b9dcadc8bbe1f41fefc0073a2f017d62051b4
khj <khj9872100@gmail.com>
2022-06-28 16:58:09 +0900

    qa 선택 불가..

--- login-com.py
+++ login-com.py
@@ -22,3 +22,48 @@ com_btn.click()
 id_box.send_keys(id)
 pw_box.send_keys(pw)
 lgn_btn.click()
+
+#공고등록 페이지 이동
+url='https://www.saramin.co.kr/zf_user/memcom/recruit/select-recruit-type'
+dvr.get(url)
+
+#일반공고
+ori = dvr.find_element(By.CSS_SELECTOR,'#content > div > div > ul > li:nth-child(2) > a')
+ori.click()
+
+# 새로 쓰기
+new = dvr.find_element(By.CSS_SELECTOR, '#btnAddRecruit')
+new.click()
+
+time.sleep(1)
+
+# 모집부문명 작성
+apply_nm=dvr.find_element(By.CSS_SELECTOR, '#recruitment_title_0')
+apply_nm.send_keys('test')
+
+# 경력 - 경력
+career = dvr.find_element(By.CSS_SELECTOR, '#line_detail_0 > div.fulldata > div:nth-child(2) > div.area_data.type_txt > span:nth-child(2) > label')
+career.click()
+
+# 근무형태
+jtype= dvr.find_element(By.CSS_SELECTOR, '#job_type_list > li:nth-child(1) > span > label')
+jtype.click()
+
+# 직무 추가
+ad=dvr.find_element(By.CSS_SELECTOR, '#jobCategory > div.area_data > div.selected_item > button.btn_add')
+ad.click()
+
+# 직무 검색
+jsearch = dvr.find_element(By.CSS_SELECTOR, '#recommend_search_input')
+jsearch.send_keys('qa')
+
+# qa 선택
+time.sleep(0.5)
+jsearch_qa = dvr.find_element(By.XPATH, '//*[@id="job_cate_search_99"]')
+jsearch_qa.click()
+jsearch_submit = dvr.find_element(By.CSS_SELECTOR, '#jobCategory > div.area_data > div.wrap_layer > div > div.area_btn > button.btnSizeL.colorBlue.btn_complete')
+jsearch_submit.click()
+
+
+
+
