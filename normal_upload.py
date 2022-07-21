76d30cb086f20027c018525e73f96d7a0642d140
khj <khj9872100@gmail.com>
2022-07-21 17:00:19 +0900

    일반공고 등록 완료!

--- normal_upload.py
+++ normal_upload.py
@@ -3,6 +3,9 @@ import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from datetime import datetime
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from sympy import false, true
 
 # id = input("id를 입력하세요: ")
 # pw = input("pw를 입력하세요: ")
@@ -12,7 +15,8 @@ pw = 'tkfkadls12!@'
 
 # 로그인창 열기
 dvr=webdriver.Chrome('./chromedriver')
-dvr.get('https://www.saramin.co.kr/zf_user/auth?url=%2Fzf_user%2F')
+url='https://www.saramin.co.kr/zf_user/auth?url=%2Fzf_user%2F'
+dvr.get(url)
 
 #로그인
 com_btn=dvr.find_element(By.XPATH,'//*[@id="login_frm"]/div[2]/div[1]/ul[1]/li[2]/a')
@@ -26,8 +30,11 @@ pw_box.send_keys(pw)
 lgn_btn.click()
 
 #공고등록 페이지 이동
-url='https://www.saramin.co.kr/zf_user/memcom/recruit/select-recruit-type'
-dvr.get(url)
+dvr.get('https://www.saramin.co.kr/zf_user/memcom/recruit/select-recruit-type')
+
+#일반 채용공고 선택
+nor=dvr.find_element(By.CSS_SELECTOR,'#btnSelectRecruit > ul > li:nth-child(1) > div > button')
+nor.click()
 
 #일반공고
 ori = dvr.find_element(By.CSS_SELECTOR,'#content > div > div > ul > li:nth-child(2) > a')
@@ -75,22 +82,45 @@ jsearch_submit.click()
 
 # 공고 제목 입력
 rec_nm=dvr.find_element(By.CSS_SELECTOR, '#title')
+
 # 월+일 4자로 보여주기 (ex. 0628)
-a=(str(datetime.today().month)+str(datetime.today().day)).zfill(4)
+# a=문자형(오늘.월).2자리 될때까지 0으로 채우기+문자형(오늘, 일).2자리 될때까지 0으로 채우기
+a=(str(datetime.today().month).zfill(2)+str(datetime.today().day).zfill(2))
+
+i = 1
+
+# 다 지우기
 rec_nm.clear()
-rec_nm.send_keys(str(a)+' 사람인 테스트 공고입니다. 지원 불가합니다.')
+#제목: a+제목+i
+rec_nm.send_keys(a+' 사람인 테스트 공고입니다. 지원 불가합니다. '+ str(i))
 
 # 템플릿 반영
 tem_apply=dvr.find_element(By.CSS_SELECTOR, '#template_insert')
 tem_apply.click()
 
-# 등록
-submit = dvr.find_element(By.CSS_SELECTOR, '#content > div.wrap_recruit_frm > div.wrap_bottom_btn > button')
-submit.click()
+# 알럿이 나오면 누르고 아님 넘겨라
+try:dvr.switch_to.alert.accept()
+except: pass
+
+while (true) :
+    # 등록 버튼 클릭 > 알럿(저기서 알럿이 뜨면 동일한 제목~~ 일듯) 뜨면 닫고, 안뜨면 반복문 끝
+    dvr.find_element(By.CSS_SELECTOR, '#content > div.wrap_recruit_frm > div.wrap_bottom_btn > button').send_keys(Keys.ENTER)
+    try:dvr.switch_to.alert.accept()
+    except: break
+
+    i+=1
+    rec_nm.clear()
+    rec_nm.send_keys(a+' 사람인 테스트 공고입니다. 지원 불가합니다. '+ str(i))
+    dvr.find_element(By.CSS_SELECTOR, '#content > div.wrap_recruit_frm > div.wrap_bottom_btn > button').send_keys(Keys.ENTER)
+
+    try:dvr.switch_to.alert.accept()
+    except: break
 
 # 등록전 확인 레이어
 # 주의: find_element하면 웹엘리먼트로 나오지만 find_elements하면 값이 리스트로 나온다
+
 sel1 = dvr.find_elements(By.CLASS_NAME, 'salaryLawItem')
+
 for i in sel1:
     i.send_keys(Keys.SPACE)
 
@@ -98,5 +128,7 @@ next=dvr.find_element(By.CSS_SELECTOR, '#submitAfterSalaryLawCheck')
 next.click()
 
 time.sleep(2)
-fin = dvr.find_element(By.CSS_SELECTOR, '#btnRecruitComplete')
-fin.click()
+# 효과높이기 - 등록
+dvr.find_element(By.CSS_SELECTOR, '#btnRecruitComplete').click()
+
+print("끝!")
